72756f57dc4b854c2d298834cab71432c7230841
Jayman Rana <jayman.code@gmail.com>
2025-06-21 23:19:38 -0400

    create list UI

--- app/src/main/java/com/jayman/fetchexercise/MainActivity.kt
+++ app/src/main/java/com/jayman/fetchexercise/MainActivity.kt
@@ -4,25 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jayman.fetchexercise.ui.theme.FetchExerciseTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FetchExerciseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    App(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { TopAppBar(title = { Text("List") }) }
+                ) { innerPadding ->
+                    ListScreen(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
                     )
                 }
             }
@@ -31,17 +43,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun App(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ListScreen(
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = modifier.fillMaxSize()) {
+        items(10) { index ->
+            ListItem(
+                modifier = Modifier.fillMaxWidth(),
+                id = index,
+                name = "Item $index",
+            )
+        }
+    }
+}
+
+@Composable
+fun ListItem(
+    id: Int,
+    name: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
         modifier = modifier
-    )
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column(Modifier.padding(16.dp)) {
+            Text(
+                text = "id: $id",
+            )
+            Text(
+                text = "name: $name",
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun AppPreview() {
+fun ListScreenPreview() {
     FetchExerciseTheme {
-        App("Android")
+        ListScreen()
     }
 }
